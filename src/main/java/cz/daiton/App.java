d94d9a4b401fa4ba470b8bb0ad73841bced45fe8
David Poslušný <david.poslusny@email.cz>
2023-07-10 15:41:52 +0200

    refactor: project basics

--- src/main/java/cz/daiton/App.java
+++ src/main/java/cz/daiton/App.java
@@ -4,9 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * JavaFX App
@@ -17,18 +19,34 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+        setProperties(stage);
+        setStage(stage);
     }
 
-    static void setRoot(String fxml) throws IOException {
+    public static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
+    }
+
+    private static Image loadIcon(String icon) throws IOException {
+        InputStream is = App.class.getResourceAsStream(icon);
+        return new Image(is);
+    }
+
+    private static void setProperties(Stage stage) throws IOException {
+        stage.setResizable(false);
+        stage.setTitle("Breakout");
+        stage.getIcons().add(loadIcon("icon.png"));
+    }
+
+    private static void setStage(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("primary"), 1000, 800);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
